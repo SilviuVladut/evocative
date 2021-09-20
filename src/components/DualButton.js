75335fac4a7736e0db9da363ea47293c33541693
SilviuVladut <71511076+SilviuVladut@users.noreply.github.com>
2021-09-20 22:01:57 +0300

    effects added on dualBtn

--- src/components/DualButton.js
+++ src/components/DualButton.js
@@ -4,10 +4,20 @@ const DualButton = () => {
   return (
     <>
       <div className="grid-item full-section-btn">
-        <div className="left-img">Women shop</div>
+        <div className="left-img">
+          <h2>
+            Shop <br /> women
+          </h2>
+          <button className="mainBtn">shop</button>
+        </div>
       </div>
       <div className="grid-item full-section-btn">
-        <div className="right-img">Man shop </div>
+        <div className="right-img">
+          <h2>
+            Shop <br /> men
+          </h2>
+          <button className="mainBtn">shop</button>
+        </div>
       </div>
       ;
     </>
